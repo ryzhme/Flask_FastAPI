92e560aa9108cc1c04e5e836f5af4b09c0a471ae
rme <rme@mail.ru>
2024-07-18 00:14:39 +0700

    lection 2
    готово

---
+++ 04_lect_2_flask/pr1.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/get/')
+def get():
+    if level := request.args.get('level'):
+        text = f'Похоже ты опытный игрок, раз имеешь уровень {level} <br> '
+    else:
+        text = 'Привет, новичок.<br>'
+    return text + f'{request.args}'
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ 04_lect_2_flask/pr3.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.get('/submit')
+def submit_get():
+    return render_template('form.html')
+
+
+@app.post('/submit')
+def submit_post():
+    name = request.form.get('name')
+    return f'Hello {name}!'
+
+
+if __name__ == '__main__':
+    app.run()
